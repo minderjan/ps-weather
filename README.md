db23df6e2f43ef2b638fb882d4acac08ac571ef8
Jan Minder <minderjan@gmail.com>
2019-11-24 15:20:34 +0100

    Loads API Key over Environment

---
+++ README.md
@@ -0,0 +1,10 @@
+# Powershell Weather Script
+
+
+## Setup Project
+
+1. Set en ENV Variable with your openweather api key
+
+```
+Set-Item -Path Env:WEATHER_KEY -Value "your-key-value"
+```
--- weather.ps1
+++ weather.ps1
@@ -1,23 +1,48 @@
 
-# https://openweathermap.org/forecast5
+# https://openweathermap.org/forecast16 (Daily Forecast)
 
-function Start-App {
-    Get-Weather-Forecast("Zürich", "CH");
+# Read API Key from Environment Variable
+$WEATHER_KEY = $env:WEATHER_KEY
+
+if ([string]::IsNullOrEmpty($WEATHER_KEY)) {
+    Write-Host "Please define a Environment Variable with name 'WEATHER_KEY' with your api key."
+} else {
+    Write-Host "Your using following api key: $WEATHER_KEY"
 }
 
+function Start-App {
+    
+    $weather = Get-Weather-Forecast("Zürich", "CH");
+    Show-Weather-Data($weather)
 
-function Get-Weather-Forecast([String]$Location, [string]$CountryCode) {
- 
-    $weather_data = Invoke-RestMethod -Uri "https://samples.openweathermap.org/data/2.5/forecast?q=$($Location),$($CountryCode)&appid=b6907d289e10d714a6e88b30761fae22"
+}
 
-    foreach ($w in $weather_data.list){
+function Get-Weather-Forecast([String]$Location, [string]$CountryCode, [int]$AmountDays = 5) {
+
+    try {
+        $result = Invoke-RestMethod -Uri "https://api.openweathermap.org/data/2.5/forecast?q=London,us&appid=$($WEATHER_KEY)"
+    } catch {
+        Write-Host "StatusCode:" $_.Exception.Response.StatusCode.value__ 
+        Write-Host "StatusDescription:" $_.Exception.Response
+    }
+
+    return $result
+
+}
+
+function Show-Weather-Data($InputWeather) {
+    foreach ($w in $InputWeather.list){
         Convert-FromUnixDate($w.dt)
      }
+}
 
+function Write-Forecast($date) {
+   
 }
 
 Function Convert-FromUnixDate ([int]$UnixDate) {
     [timezone]::CurrentTimeZone.ToLocalTime(([datetime]'1/1/1970').AddSeconds($UnixDate))
- }
+}
+
 
 Start-App
